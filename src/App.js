05728f98cb6c19c1ff17d2eac0487296df20b805
Emmanuel AKINDE <ibukunbamise@yahoo.com>
2018-01-07 19:07:44 +0100

    basic routes with BrowserRouter

--- src/App.js
+++ src/App.js
@@ -1,3 +1,23 @@
 import React from 'react';
-const App = () => <h1>hello</h1>
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+const Home = (props) => {
+  console.log(props);
+return(
+<h1>Hello World</h1>
+)
+}
+const App = () => {
+  return (
+    <Router>
+      <div>
+        <Route exact path="/" component={Home} />
+        {/*<Route strict path="/about" component={Home} />*/} {/* using strict to confirm exact route regexp with strict  */}
+        {/* <Route path="/about" render={() => <h1>about</h1>} /> */}
+        <Route path="/about" children={(match) =>  match && <h1>about</h1>} /> 
+        {/* this is information that are passed through render or 
+        children from react-router to our components */}
+      </div>
+    </Router> 
+  )
+}
 export default App;

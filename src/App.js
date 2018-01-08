269f06d8579d20e1b5411c4e3304a8d8423c4df5
Emmanuel AKINDE <ibukunbamise@yahoo.com>
2018-01-08 06:40:02 +0100

    The redirect exmple

--- src/App.js
+++ src/App.js
@@ -1,35 +1,40 @@
-import React from 'react';
-import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
-// const Home = (props) => {
-//   console.log(props);
-// return(
-// <h1>Hello World</h1>
-// )
-//}
-const Links  = (props) => {
-  return(
+import React from 'react'
+import {
+    BrowserRouter as Router,
+    Route,
+    Link,
+    Switch,
+    Redirect
+} from 'react-router-dom';
+import './App.css';
+const loggedIn = true;
+const Links = () => (
     <nav>
-      <Link to="/">Home </Link>
-      <Link to={{ pathname: '/about'}}> About</Link>
-      <Link replace to="/contact">Contact</Link> {/* replace gives us a little control of how we managing our browser history, it replaces the previous URL */}
+        <Link to="/">Home</Link>
+        <Link to="/old/123">Old</Link>
+        <Link to="/new/456">New</Link> 
+        <Link to="/protected">Protected</Link>       
     </nav>
-  )
-}
+)
 
 const App = () => {
-  return (
-    <Router>
-      <div>
-        <Links/>
-        <Route exact path="/" render={() => <h1>Hello World</h1>} />
-        {/*<Route strict path="/about" component={Home} />*/} {/* using strict to confirm exact route regexp with strict  */}
-        <Route path="/about" render={() => <h1>About</h1>} />
-        {/* <Route path="/about" children={(match) =>  match && <h1>About</h1>} />  */}
-        {/* this is information that are passed through render or 
-        children from react-router to our components */}
-        <Route path="/contact" render={() => <h1>Contact</h1>} />
-      </div>
-    </Router> 
-  )
+    return (
+        <Router>
+            <div>
+                <Links />
+                    <Route exact path="/" render={() => (<h1>Home</h1>)}/>
+                    <Route exact path="/new/:str" render={({match}) => (<h1>New: {match.params.str}</h1>)}/>
+                    <Route path="/old/:str" render={({match}) => (
+                        <Redirect to={`/new/${match.params.str}`} />
+                    )} /> 
+                    <Route path="/protected" render={() => (
+                        loggedIn
+                        ?  <h1>Welcome ro the protected page</h1>
+                        :<Redirect to="/new/login" />
+                    )} /> 
+            </div>
+        </Router>
+    )
 }
+
 export default App;
